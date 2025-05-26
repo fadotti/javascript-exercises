735f4dc80d9c2b744f82a4a6030bdd455c1f0cc9
Federico Dotti <fede.dotti.94@gmail.com>
2025-05-26 18:57:19 +0000

    Solve 06 and 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splittedString = string.split('');
+    let output = '';
+    for(i = splittedString.length - 1; i >= 0; i--) {
+        output += splittedString[i];
+    }
+    return output
 };
 
 // Do not edit below this line
