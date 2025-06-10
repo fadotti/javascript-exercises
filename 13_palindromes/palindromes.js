bcc49390c7952af1165125caf5238a8bafd0b0da
Federico Dotti <fede.dotti.94@gmail.com>
2025-06-10 22:19:15 +0000

    Solve 12 and 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let newString = string.toLowerCase();
+  let stringAsArray = newString.split("");
+  let filteredString = stringAsArray.filter((character) => {
+    return (character == "!" || character == "." || character == " " || character == ",") ? false : true
+  })
 
+  let output = filteredString.reduce((accumulator, currentValue, currentIndex, array) => {
+    if(accumulator == false) {
+      return false
+    } else {
+      return (currentValue == array[array.length - 1 - currentIndex]) ? true : false
+    }
+  }, true)
+
+  return output
 };
 
 // Do not edit below this line
