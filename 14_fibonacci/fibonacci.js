f7179758c18c716c457f96154dbb418f3c1685da
Federico Dotti <fede.dotti.94@gmail.com>
2025-06-12 02:44:48 +0000

    Solve 14 thru 16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    switch(true) {
+        case input == 0:
+            return 0
+        case input < 0:
+            return "OOPS"
+        case input < 3:
+            return 1
+        default:
+            let fibonacciArray = [1, 1];
+            for(let i = 2; i < input; i++) {
+                fibonacciArray.push(fibonacciArray[i-2] + fibonacciArray[i-1])
+            }
+            return fibonacciArray[input - 1]
+    }
 
 };
 
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    peopleProcessed = people.map((person) => {
+        let currentDate = new Date();
+        let year = currentDate.getFullYear();
 
+        let name = person.name;
+        let age = ("yearOfDeath" in person) ? person.yearOfDeath - person.yearOfBirth : year - person.yearOfBirth;
+
+        return {name, age}
+    })
+
+    peopleProcessed.sort((a, b) => a.age - b.age);
+    
+    return peopleProcessed.at(-1)
 };
 
 // Do not edit below this line
